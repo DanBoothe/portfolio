adc935a0cef4ab18f2bb366bdfae7bedf23172f9
DanBoothe <100175408+DanBoothe@users.noreply.github.com>
2023-11-25 10:33:12 -0700

    Added resume html

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 const openNav = () => {
     document.getElementById("mySidebar").style.width = "25rem";
-    document.getElementById("main").style.marginLeft = "25rem";
+    document.getElementById("menu").style.marginLeft = "25rem";
     document.getElementById("open").style.display = "none";
+    document.getElementById("logo-container").style.display = "inline-block";
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
   const closeNav = () => {
     document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("menu").style.marginLeft = "0";
     document.getElementById("open").style.display = "block";
+    document.getElementById("logo-container").style.display = "none";
   }
